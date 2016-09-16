19caf9bee14d91a2cb105f85cc98b5f5a87b26a7
Ashok Rajpurohit <ashok1113@hotmail.com>
2016-09-16 12:56:13 +0530

    hackerRank problems

--- src/com/ashok/hackerRank/NumberTheory/ManasaMaths.java
+++ src/com/ashok/hackerRank/NumberTheory/ManasaMaths.java
@@ -1,20 +1,65 @@
 package com.ashok.hackerRank.NumberTheory;
 
+import com.ashok.lang.dsa.RandomStrings;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 /**
- * Problem Name:
- * Link:
+ * Problem Name: Manasa loves Maths
+ * Link: https://www.hackerrank.com/challenges/manasa-loves-maths
  *
  * @author Ashok Rajpurohit (ashok1113@gmail.com)
  */
 public class ManasaMaths {
     private static PrintWriter out = new PrintWriter(System.out);
     private static InputReader in = new InputReader();
+    private static boolean[][][] map3 = new boolean[10][10][10];
+    private static boolean[][] map2 = new boolean[10][10];
+    private static int[] charToDigits = new int[256];
+
+    static {
+        Arrays.fill(charToDigits, -1);
+
+        for (int i = '0'; i <= '9'; i++)
+            charToDigits[i] = i - '0';
+
+        map3[0][0][0] = true;
+        map2[0][0] = true;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    int num = (10 * i + j) * 10 + k;
+                    boolean res = (num & 7) == 0;
+
+                    if (!res)
+                        continue;
+
+                    map3[i][j][k] = res;
+                    map3[i][k][j] = res;
+                    map3[j][i][k] = res;
+                    map3[j][k][i] = res;
+                    map3[k][i][j] = res;
+                    map3[k][j][i] = res;
+                }
+
+                int num = 10 * i + j;
+                boolean res = (num & 7) == 0;
+
+                if (!res)
+                    continue;
+
+                map2[i][j] = res;
+                map2[j][i] = res;
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
+        test(in.readInt(), in.readInt());
         ManasaMaths a = new ManasaMaths();
         a.solve();
         in.close();
@@ -22,10 +67,95 @@ public class ManasaMaths {
     }
 
     private void solve() throws IOException {
-        while (true) {
-            out.println(in.readLine());
-            out.flush();
+        int t = in.readInt();
+        StringBuilder sb = new StringBuilder(t << 2);
+        String yes = "YES\n", no = "NO\n";
+
+        while (t > 0) {
+            t--;
+
+            if (process(in.read()))
+                sb.append(yes);
+            else
+                sb.append(no);
         }
+
+        out.print(sb);
+    }
+
+    private static void test(int testCount, int size) throws IOException {
+        RandomStrings randomStrings = new RandomStrings();
+
+        while (testCount > 0) {
+//            testCount--;
+
+            String s = randomStrings.nextString123(in.readInt());
+            System.out.println(s.length());
+
+            long time = System.currentTimeMillis();
+            process(s);
+            System.out.println(System.currentTimeMillis() - time);
+        }
+    }
+
+    private static boolean process(String s) {
+        char[] digits = formArray(s);
+
+        if (allOdds(digits))
+            return false;
+
+        if (digits.length == 1)
+            return (charToDigits[digits[0]] & 7) == 0;
+
+        if (digits.length == 2)
+            return map2[charToDigits[digits[0]]][charToDigits[digits[1]]];
+
+        for (int i = 0; i < digits.length; i++)
+            for (int j = i + 1; j < digits.length; j++)
+                for (int k = j + 1; k < digits.length; k++)
+                    if (check(digits[i], digits[j], digits[k]))
+                        return true;
+
+        return false;
+    }
+
+    private static boolean allOdds(char[] ar) {
+        for (char e : ar) {
+            if ((charToDigits[e] & 1) == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    private static char[] formArray(String s) {
+        char[] ar = s.toCharArray();
+        int[] map = new int[256];
+
+        for (char e : ar)
+            map[e]++;
+
+        int count = 0;
+        for (int i = '0'; i <= '9'; i++)
+            if (map[i] > 3) {
+                count += 3;
+                map[i] = 3;
+            } else
+                count += map[i];
+
+        char[] res = new char[count];
+        for (int i = 0, j = '0'; i < count && j <= '9'; j++) {
+            while (map[j] != 0) {
+                res[i++] = (char) j;
+                map[j]--;
+            }
+        }
+
+        return res;
+    }
+
+    private static boolean check(char a, char b, char c) {
+        return map3[charToDigits[a]][charToDigits[b]][charToDigits[c]];
     }
 
     final static class InputReader {
@@ -70,51 +200,6 @@ public class ManasaMaths {
             return number * s;
         }
 
-        public int[] readIntArray(int n) throws IOException {
-            int[] ar = new int[n];
-            for (int i = 0; i < n; i++)
-                ar[i] = readInt();
-
-            return ar;
-        }
-
-        public long readLong() throws IOException {
-            long res = 0;
-            int s = 1;
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-            for (; buffer[offset] < 0x30 || buffer[offset] == '-'; ++offset) {
-                if (buffer[offset] == '-')
-                    s = -1;
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (; offset < bufferSize && buffer[offset] > 0x2f; ++offset) {
-                res = (res << 3) + (res << 1) + buffer[offset] - 0x30;
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            ++offset;
-            if (s == -1)
-                res = -res;
-            return res;
-        }
-
-        public long[] readLongArray(int n) throws IOException {
-            long[] ar = new long[n];
-
-            for (int i = 0; i < n; i++)
-                ar[i] = readLong();
-
-            return ar;
-        }
-
         public String read() throws IOException {
             StringBuilder sb = new StringBuilder();
             if (offset == bufferSize) {
@@ -134,71 +219,6 @@ public class ManasaMaths {
                 }
             }
             for (; offset < bufferSize; ++offset) {
-                if (buffer[offset] == ' ' || buffer[offset] == '\t' ||
-                        buffer[offset] == '\n' || buffer[offset] == '\r')
-                    break;
-                if (Character.isValidCodePoint(buffer[offset])) {
-                    sb.appendCodePoint(buffer[offset]);
-                }
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            return sb.toString();
-        }
-
-        public String readLine() throws IOException {
-            StringBuilder sb = new StringBuilder();
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-
-            if (bufferSize == -1 || bufferSize == 0)
-                throw new IOException("No new bytes");
-
-            for (;
-                 buffer[offset] == ' ' || buffer[offset] == '\t' || buffer[offset] ==
-                         '\n' || buffer[offset] == '\r'; ++offset) {
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (; offset < bufferSize; ++offset) {
-                if (buffer[offset] == '\n' || buffer[offset] == '\r')
-                    break;
-                if (Character.isValidCodePoint(buffer[offset])) {
-                    sb.appendCodePoint(buffer[offset]);
-                }
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            return sb.toString();
-        }
-
-        public String read(int n) throws IOException {
-            StringBuilder sb = new StringBuilder(n);
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-
-            if (bufferSize == -1 || bufferSize == 0)
-                throw new IOException("No new bytes");
-
-            for (;
-                 buffer[offset] == ' ' || buffer[offset] == '\t' || buffer[offset] ==
-                         '\n' || buffer[offset] == '\r'; ++offset) {
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (int i = 0; offset < bufferSize && i < n; ++offset) {
                 if (buffer[offset] == ' ' || buffer[offset] == '\t' ||
                         buffer[offset] == '\n' || buffer[offset] == '\r')
                     break;
--- src/com/ashok/hackerRank/stack/Waiter.java
+++ src/com/ashok/hackerRank/stack/Waiter.java
@@ -3,16 +3,30 @@ package com.ashok.hackerRank.stack;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
- * Problem Name:
- * Link:
+ * Problem Name: Waiter
+ * Link: https://www.hackerrank.com/challenges/waiter
  *
  * @author Ashok Rajpurohit (ashok1113@gmail.com)
  */
 public class Waiter {
     private static PrintWriter out = new PrintWriter(System.out);
     private static InputReader in = new InputReader();
+    private static boolean[] primes = new boolean[10000];
+
+    static {
+        Arrays.fill(primes, true);
+        for (int i = 2; i < primes.length; i++) {
+            if (!primes[i])
+                continue;
+
+            for (int j = i + i; j < primes.length; j += i)
+                primes[j] = false;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         solve();
@@ -21,10 +35,38 @@ public class Waiter {
     }
 
     private static void solve() throws IOException {
-        while (true) {
-            out.println(in.readLine());
-            out.flush();
+        int n = in.readInt(), q = in.readInt();
+        int[] plates = in.readIntArray(n);
+        StringBuilder sb = new StringBuilder(n << 2);
+        LinkedList<Integer> list = new LinkedList<>(), rem = new LinkedList<>();
+        for (int e : plates)
+            rem.add(e);
+
+        int prime = 2;
+        while (q > 0 && rem.size() > 0) {
+            while (!primes[prime])
+                prime++;
+
+            LinkedList<Integer> temp = new LinkedList<>();
+            for (int e : rem) {
+                if (e % prime == 0)
+                    list.add(e);
+                else
+                    temp.addFirst(e);
+            }
+
+            q--;
+            prime++;
+            rem = temp;
         }
+
+        for (int e : list)
+            sb.append(e).append('\n');
+
+        while (rem.size() != 0)
+            sb.append(rem.removeLast()).append('\n');
+
+        out.print(sb);
     }
 
     final static class InputReader {
@@ -75,141 +117,6 @@ public class Waiter {
                 ar[i] = readInt();
 
             return ar;
-        }
-
-        public long readLong() throws IOException {
-            long res = 0;
-            int s = 1;
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-            for (; buffer[offset] < 0x30 || buffer[offset] == '-'; ++offset) {
-                if (buffer[offset] == '-')
-                    s = -1;
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (; offset < bufferSize && buffer[offset] > 0x2f; ++offset) {
-                res = (res << 3) + (res << 1) + buffer[offset] - 0x30;
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            ++offset;
-            if (s == -1)
-                res = -res;
-            return res;
-        }
-
-        public long[] readLongArray(int n) throws IOException {
-            long[] ar = new long[n];
-
-            for (int i = 0; i < n; i++)
-                ar[i] = readLong();
-
-            return ar;
-        }
-
-        public String read() throws IOException {
-            StringBuilder sb = new StringBuilder();
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-
-            if (bufferSize == -1 || bufferSize == 0)
-                throw new IOException("No new bytes");
-
-            for (;
-                 buffer[offset] == ' ' || buffer[offset] == '\t' || buffer[offset] ==
-                         '\n' || buffer[offset] == '\r'; ++offset) {
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (; offset < bufferSize; ++offset) {
-                if (buffer[offset] == ' ' || buffer[offset] == '\t' ||
-                        buffer[offset] == '\n' || buffer[offset] == '\r')
-                    break;
-                if (Character.isValidCodePoint(buffer[offset])) {
-                    sb.appendCodePoint(buffer[offset]);
-                }
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            return sb.toString();
-        }
-
-        public String readLine() throws IOException {
-            StringBuilder sb = new StringBuilder();
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-
-            if (bufferSize == -1 || bufferSize == 0)
-                throw new IOException("No new bytes");
-
-            for (;
-                 buffer[offset] == ' ' || buffer[offset] == '\t' || buffer[offset] ==
-                         '\n' || buffer[offset] == '\r'; ++offset) {
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (; offset < bufferSize; ++offset) {
-                if (buffer[offset] == '\n' || buffer[offset] == '\r')
-                    break;
-                if (Character.isValidCodePoint(buffer[offset])) {
-                    sb.appendCodePoint(buffer[offset]);
-                }
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            return sb.toString();
-        }
-
-        public String read(int n) throws IOException {
-            StringBuilder sb = new StringBuilder(n);
-            if (offset == bufferSize) {
-                offset = 0;
-                bufferSize = in.read(buffer);
-            }
-
-            if (bufferSize == -1 || bufferSize == 0)
-                throw new IOException("No new bytes");
-
-            for (;
-                 buffer[offset] == ' ' || buffer[offset] == '\t' || buffer[offset] ==
-                         '\n' || buffer[offset] == '\r'; ++offset) {
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            for (int i = 0; offset < bufferSize && i < n; ++offset) {
-                if (buffer[offset] == ' ' || buffer[offset] == '\t' ||
-                        buffer[offset] == '\n' || buffer[offset] == '\r')
-                    break;
-                if (Character.isValidCodePoint(buffer[offset])) {
-                    sb.appendCodePoint(buffer[offset]);
-                }
-                if (offset == bufferSize - 1) {
-                    offset = -1;
-                    bufferSize = in.read(buffer);
-                }
-            }
-            return sb.toString();
         }
     }
 }
