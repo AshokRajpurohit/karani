11d3bc7b2e83d6f3346f8318ef1d8705187d5419
Ashok Rajpurohit <ashok1113@hotmail.com>
2016-09-18 14:47:42 +0530

    updated code description.

---
+++ src/com/ashok/topcoder/contest/SRM698.java
@@ -0,0 +1,7 @@
+package com.ashok.topcoder.contest;
+
+/**
+ * @author Ashok Rajpurohit (ashok1113@gmail.com).
+ */
+public class SRM698 {
+}
---
+++ src/com/ashok/topcoder/practice/LittleElephantAndString.java
@@ -0,0 +1,7 @@
+package com.ashok.topcoder.practice;
+
+/**
+ * @author Ashok Rajpurohit (ashok1113@gmail.com).
+ */
+public class LittleElephantAndString {
+}
