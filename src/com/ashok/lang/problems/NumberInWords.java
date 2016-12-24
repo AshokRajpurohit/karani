8657c09a3da58ca1f16628859508cdc80c65e8a8
Ashok Rajpurohit <ashok1113@hotmail.com>
2016-12-24 21:30:13 +0530

    Corrected typo in one word. Fourty to Forty

--- src/com/ashok/lang/problems/NumberInWords.java
+++ src/com/ashok/lang/problems/NumberInWords.java
@@ -20,7 +20,7 @@ public class NumberInWords {
     { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
       "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
       "Sixteen", "Seventeen", "Eighteen", "Nineteen" }, TENS =
-    { "Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy",
+    { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
       "Eighty", "Ninety" };
 
     private final static String[] numbers =
